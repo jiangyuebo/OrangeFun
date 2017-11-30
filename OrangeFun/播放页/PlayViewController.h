6761a8f5b6afd98acd6f38196a705834859d2c3a
qiflr <jiangyuebo@saicmotor.com>
2017-12-01 01:45:10 +0800

    add play page

--- OrangeFun/播放页/PlayViewController.h
+++ OrangeFun/播放页/PlayViewController.h
@@ -10,4 +10,25 @@
 
 @interface PlayViewController : UIViewController
 
+//进度条
+@property (strong, nonatomic) IBOutlet UISlider *progressSlider;
+
+//左时间
+@property (strong, nonatomic) IBOutlet UILabel *progressTime;
+
+//右时间
+@property (strong, nonatomic) IBOutlet UILabel *totalTime;
+
+//封面背景
+@property (strong, nonatomic) IBOutlet UIImageView *coverDiskImageView;
+
+//播放、暂停按钮
+@property (strong, nonatomic) IBOutlet UIButton *playControlButton;
+
+//上一首
+@property (strong, nonatomic) IBOutlet UIButton *prevButton;
+
+//下一首
+@property (strong, nonatomic) IBOutlet UIButton *nextButton;
+
 @end
--- OrangeFun/播放页/PlayViewController.m
+++ OrangeFun/播放页/PlayViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "PlayViewController.h"
+#import "globalHeader.h"
+#import "JerryViewTools.h"
 
 @interface PlayViewController ()
 
@@ -17,6 +19,55 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    UIImage *testImage = [UIImage imageNamed:@"xiaozhu"];
+    //背景
+    [self addBackgroundView:testImage];
+    
+    [self initView];
+}
+
+- (void)viewDidLayoutSubviews{
+    UIImage *testImage = [UIImage imageNamed:@"xiaozhu"];
+    //封面
+    [self setCoverInDisk:testImage];
+}
+
+#pragma mark 初始化一些view
+- (void)initView{
+    //进度条设置
+    UIImage *minImage = [[UIImage imageNamed:@"progress_orange"] resizableImageWithCapInsets:UIEdgeInsetsZero resizingMode:UIImageResizingModeTile];
+    UIImage *maxImage = [[UIImage imageNamed:@"progress"] resizableImageWithCapInsets:UIEdgeInsetsZero resizingMode:UIImageResizingModeTile];
+    
+    [self.progressSlider setMinimumTrackImage:minImage forState:UIControlStateNormal];
+    [self.progressSlider setMaximumTrackImage:maxImage forState:UIControlStateNormal];
+    [self.progressSlider setThumbImage:[UIImage imageNamed:@"drag"] forState:UIControlStateNormal];
+}
+
+#pragma mark 设置圆形封面图片
+- (void)setCoverInDisk:(UIImage *) cover{
+    UIImageView *coverImageView = [[UIImageView alloc] initWithImage:cover];
+    [coverImageView setFrame:CGRectMake(7, 7, self.coverDiskImageView.frame.size.width - 14, self.coverDiskImageView.frame.size.height - 14)];
+    coverImageView.layer.cornerRadius = coverImageView.frame.size.width / 2;//裁成圆角
+    coverImageView.layer.masksToBounds = YES;//隐藏裁剪掉的部分
+    
+    [self.coverDiskImageView addSubview:coverImageView];
+}
+
+#pragma mark 设置背景图（封面高斯模糊 + 20%黑色遮罩）
+- (void)addBackgroundView:(UIImage *) coverImage {
+    //高斯模糊
+    UIImage *gaussianImage = [JerryViewTools coreBlurImage:coverImage withBlurNumber:20];
+    UIImageView *backgroundImageView = [[UIImageView alloc] initWithFrame:CGRectMake(- SCREENWIDTH/2, - SCREENHEIGHT/2, 2 * SCREENWIDTH, 2 * SCREENHEIGHT)];
+    [backgroundImageView setImage:gaussianImage];
+    
+    //黑色遮罩
+    UIView *blackMaskView = [[UIView alloc] initWithFrame:backgroundImageView.frame];
+    [blackMaskView setBackgroundColor:[UIColor blackColor]];
+    [blackMaskView setAlpha:0.2];
+
+    [backgroundImageView addSubview:blackMaskView];
+    [self.view addSubview:backgroundImageView];
+    [self.view sendSubviewToBack:backgroundImageView];
 }
 
 - (void)viewDidAppear:(BOOL)animated{
